a4916c93e2b16514a18ff41c3611b97135eb2eff
Mouflon <54674592+Mouflon23@users.noreply.github.com>
2025-02-13 21:31:38 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://scriva-osteopathe.fr',
+  output: 'static',  // Ensure Astro outputs a static site
+  outDir: 'dist',    // This must match your GitHub workflow config
 });
