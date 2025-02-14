a98be9d5a7a08bf0428bcef9d56d17d9b5adae02
Mouflon23 <sbdorne@gmail.com>
2025-02-14 11:39:12 +0100

    connecting css

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,ts,tsx}'],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  primary: 'var(--primary)',
+		  secondary: 'var(--secondary)',
+		  accent: 'var(--accent)',
+		},
+	  },
 	},
 	plugins: [],
-}
+  };
+  
